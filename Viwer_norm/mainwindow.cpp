cbfc7dbad47812c1b2986acd2feec18b77a3c1f3
Fixie Radish <fixierad@student.21-school.ru>
2023-01-28 22:06:30 +0300

    sad

--- Viwer_norm/mainwindow.cpp
+++ Viwer_norm/mainwindow.cpp
@@ -16,7 +16,7 @@ void MainWindow::on_OpenobjFile_clicked() {
 
 void MainWindow::on_EditFile_clicked()
 {
-     QString str = QFileDialog::getOpenFileName(this,"Open file","/Users/fixierad/Projects/3D_viewer_C", "*.obj");
+     QString str = QFileDialog::getOpenFileName(this,"Open file","/Users/", "*.obj");
      std::string v_str = str.toStdString();
        const char* strch = v_str.c_str();
        auto s = const_cast<char*>(strch);
